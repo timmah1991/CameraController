39b620f32ea3201077f24ad9624f2c0dd9d1140f
Thomas Kittredge <thomaskittredge@Thomass-MBP.localdomain>
2019-01-10 22:53:32 -0700

    fixed some typos

--- firmware/CameraControl/src/CameraControl.ino
+++ firmware/CameraControl/src/CameraControl.ino
@@ -14,9 +14,9 @@ int shutterCamera2(String command);
 
 void setup() {
   pinMode(c1shutter, OUTPUT);
-  pinMode(c1focus, OUTPUT):
+  pinMode(c1focus, OUTPUT);
   pinMode(c2shutter, OUTPUT);
-  pinMode(c2focus, OUTPUT):
+  pinMode(c2focus, OUTPUT);
   Particle.function("shutter1", shutterCamera1);
   Particle.function("shutter2", shutterCamera2);
 }
@@ -28,17 +28,17 @@ void loop() {
 int shutterCamera1(String command)
 {
   int shutterDelay1 = command.toInt();
-  digitalWrite(c1Shutter, HIGH);
+  digitalWrite(c1shutter, HIGH);
   delay(shutterDelay1);
-  digitalWrite(c1Shutter, LOW);
+  digitalWrite(c1shutter, LOW);
   return 1;
 }
 
 int shutterCamera2(String command)
 {
   int shutterDelay2 = command.toInt();
-  digitalWrite(c2Shutter, HIGH);
+  digitalWrite(c2shutter, HIGH);
   delay(shutterDelay2);
-  digitalWrite(c2Shutter, LOW);
+  digitalWrite(c2shutter, LOW);
   return 1;
 }
